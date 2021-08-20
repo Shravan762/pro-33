987efb77cb73421a6e4f60da0b4aab07a89d4c60
Shravan762 <75240159+Shravan762@users.noreply.github.com>
2021-08-20 11:17:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,29 +2,48 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+var Background
+var Snow1
+var SnowImg
+var Character;
+var CharacterImg;
+var Snowman;
+var SnowmanImg;
+var Snowparticle=[];
+function preload(){
+Background = loadImage("snow2.jpg");
+CharacterImg = loadImage("Character.png");
+SnowImg = loadImage("Snowman.png");
+}
 
-
- var bg= "snow3.jpg"  
- var snow 
- var backgroundImg    
-           
-  function preload(){
-    getBackgroundImg()
-  }
-
-   function setup() {
+function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
 
-  snow = new Snow(800,50)
+      
+  Character = createSprite(540,350,10,10);
+  Character.addImage(CharacterImg);
+  Character.scale = 0.3;
+
+  Snowman = createSprite(260,300,10,10);
+  Snowman.addImage(SnowImg);
+  Snowman.scale = 0.3;
+
 }
 
 function draw() {
+  background(Background);  
+  Engine.update(engine);
+  if(frameCount%20===0){
+    Snowparticle.push(new Snow(random(100, 700), -50,10));
 
-  if(backgroundImg)
-        background(backgroundImg);
-        
-        snow.display()
-        
+  }
+
+  for (var j = 0; j < Snowparticle.length; j++) {
+   
+    Snowparticle[j].display();
+  }
+ 
   drawSprites();
 }
